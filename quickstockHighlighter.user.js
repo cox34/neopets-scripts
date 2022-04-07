954ea28460fed7757218467d52d29f78ff752110
cox34 <17260496+cox34@users.noreply.github.com>
2022-04-07 15:15:46 +0000

    Create quickstockHighlighter.user.js

---
+++ quickstockHighlighter.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name        Neopets - Quickstock Highlighter
+// @namespace   Violentmonkey Scripts
+// @match       https://www.neopets.com/quickstock.phtml
+// @grant       none
+// @version     1.0
+// @author      -
+// @description Highlights items in quickstock
+// ==/UserScript==
+
+const highlightColor1 = "gold";
+const highlightedItems1 = [
+  
+  "Nerkmid"
+  
+];
+const highlightColor2 = "tan";
+const highlightedItems2 = [
+  
+  "Codestone",
+  "Bubbling Fungus"
+  
+];
+
+const quickstockItems = document.querySelector("form[name='quickstock']").querySelectorAll("td");
+
+for(let row of quickstockItems){
+  if(highlightedItems1.some(e => row.textContent.includes(e))) {
+    row.parentElement.style.backgroundColor = highlightColor1;
+  }
+}
+for(let row of quickstockItems){
+  if(highlightedItems2.some(e => row.textContent.includes(e))) {
+    row.parentElement.style.backgroundColor = highlightColor2;
+  }
+}
