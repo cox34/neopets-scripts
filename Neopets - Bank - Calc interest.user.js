1d0e95987402f86e7057f7059631577ead0e7a80
cox34 <17260496+cox34@users.noreply.github.com>
2022-06-11 05:31:25 +0000

    Update Neopets - Bank - Calc interest.user.js

--- Neopets - Bank - Calc interest.user.js
+++ Neopets - Bank - Calc interest.user.js
@@ -10,23 +10,13 @@
 
 const interestYearly = parseInt(document.querySelector("#txtAnnualInterest").textContent.replace(/,/g,""));
 const interestDaily = Math.ceil(interestYearly/365);
-const interestWeekly = interestDaily*7;
-const interestMonthly = interestDaily*30;
 
-const dailyInterestContainer = document.createElement("div");
-dailyInterestContainer.textContent = "Daily Interest: "+interestDaily.toLocaleString("en-US")+" NP";
-dailyInterestContainer.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
-dailyInterestContainer.style.textAlign = "center";
-document.querySelector(".bank-grid2").parentNode.insertBefore(dailyInterestContainer, document.querySelector(".bank-grid2").nextSibling);
-
-const weeklyInterestContainer = document.createElement("div");
-weeklyInterestContainer.textContent = "7 Days Interest: "+interestWeekly.toLocaleString("en-US")+" NP";
-weeklyInterestContainer.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
-weeklyInterestContainer.style.textAlign = "center";
-document.querySelector(".bank-grid2").parentNode.insertBefore(weeklyInterestContainer, document.querySelector(".bank-grid2").nextSibling);
-
-const monthlyInterestContainer = document.createElement("div");
-monthlyInterestContainer.textContent = "30 Days Interest: "+interestMonthly.toLocaleString("en-US")+" NP";
-monthlyInterestContainer.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
-monthlyInterestContainer.style.textAlign = "center";
-document.querySelector(".bank-grid2").parentNode.insertBefore(monthlyInterestContainer, document.querySelector(".bank-grid2").nextSibling);
+for(const [index, int] of [interestDaily, interestDaily*7, interestDaily*30].entries()){
+  const div = document.createElement("div");
+  div.textContent = ["Daily", "7-day", "30-day"][index]+" Interest: "+int.toLocaleString("en-US")+" NP";
+  div.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
+  div.style.textAlign = "center";
+  document.querySelector(".bank-grid2").parentNode.insertBefore(
+    div, document.querySelector(".bank-grid2").nextSibling
+  );
+}
