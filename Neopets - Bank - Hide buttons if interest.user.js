dd4ddb08d357838b4c43c0757633fe405c8fbad2
cox34 <17260496+cox34@users.noreply.github.com>
2022-06-11 05:17:27 +0000

    Update Neopets - Bank - Hide buttons if interest.user.js

--- Neopets - Bank - Hide buttons if interest.user.js
+++ Neopets - Bank - Hide buttons if interest.user.js
@@ -5,34 +5,23 @@
 // @match       *://www.neopets.com/bank.phtml
 // @grant       none
 // @version     1.0
-// @description Removes withdraw and deposit options until interest is collected, shows daily, 7day, 30day interest
+// @description Removes withdraw and deposit options until interest is collected
 // ==/UserScript==
 
-const interestYearly = parseInt(document.querySelector("#txtAnnualInterest").textContent.replace(/,/g,""));
-const interestDaily = Math.ceil(interestYearly/365);
-const interestWeekly = interestDaily*7;
-const interestMonthly = interestDaily*30;
-
-const dailyInterestContainer = document.createElement("div");
-dailyInterestContainer.textContent = "Daily Interest: "+interestDaily.toLocaleString("en-US")+" NP";
-dailyInterestContainer.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
-dailyInterestContainer.style.textAlign = "center";
-document.querySelector(".bank-grid2").parentNode.insertBefore(dailyInterestContainer, document.querySelector(".bank-grid2").nextSibling);
-
-const weeklyInterestContainer = document.createElement("div");
-weeklyInterestContainer.textContent = "7 Days Interest: "+interestWeekly.toLocaleString("en-US")+" NP";
-weeklyInterestContainer.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
-weeklyInterestContainer.style.textAlign = "center";
-document.querySelector(".bank-grid2").parentNode.insertBefore(weeklyInterestContainer, document.querySelector(".bank-grid2").nextSibling);
-
-const monthlyInterestContainer = document.createElement("div");
-monthlyInterestContainer.textContent = "30 Days Interest: "+interestMonthly.toLocaleString("en-US")+" NP";
-monthlyInterestContainer.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
-monthlyInterestContainer.style.textAlign = "center";
-document.querySelector(".bank-grid2").parentNode.insertBefore(monthlyInterestContainer, document.querySelector(".bank-grid2").nextSibling);
-
 const btnCollectInt = document.querySelector(".bank-interest-btn");
-if(btnCollectInt.value = "Collect Interest"){
-  document.querySelectorAll(".bank-input-grid")[0].style.visibility="hidden";
-  document.querySelectorAll(".bank-input-grid")[1].style.visibility="hidden";
+if(btnCollectInt.value === "Collect Interest"){
+  for(const [index, el] of document.querySelectorAll(".bank-input-grid").entries()){
+    el.style.visibility="hidden";
+    const warning = document.createElement("div");
+    warning.textContent = "You have uncollected interest. Click here to unhide the input."
+    warning.style.fontFamily = "\"MuseoSansRounded700\", \'Arial\', sans-serif";
+    warning.style.cursor = "pointer";
+    warning.style.paddingTop = "10px";
+    warning.style.textAlign = "center";
+    warning.addEventListener("click", ()=>{
+      document.querySelectorAll(".bank-input-grid")[index].style.visibility="visible";
+      event.target.style.display = "none";
+    });
+    el.parentNode.prepend(warning);
+  }
 }
