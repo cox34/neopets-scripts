07ef52cfbefa25fcd412ce865f281b757ad90e2a
cox34 <17260496+cox34@users.noreply.github.com>
2022-06-24 11:34:14 +0000

    Create Neopets - Wishing Well - Wish.user.js

---
+++ Neopets - Wishing Well - Wish.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Neopets - Wishing Well
+// @author      cox34
+// @namespace   https://github.com/cox34/neopets-scripts
+// @match       https://www.neopets.com/wishing.phtml
+// @grant       none
+// @version     1.0
+// @description Input wishing well wish
+// ==/UserScript==
+
+document.getElementsByName("donation")[0].value = 21;
+document.getElementsByName("wish")[0].value = "Neopets 22nd Birthday Goodie Bag";
